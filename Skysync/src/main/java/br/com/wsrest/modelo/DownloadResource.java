2efdaf89481907d56960df5add991196ce14d391
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2016-08-19 13:57:47 -0300

    feito atualizacao para metodos de upload e Download do projeto.

---
+++ Skysync/src/main/java/br/com/wsrest/modelo/DownloadResource.java
@@ -0,0 +1,47 @@
+/**
+ * @author RodrigoTenorio
+ */
+package br.com.wsrest.modelo;
+
+import java.io.File;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+@Path("/download")
+public class DownloadResource {
+	private File file;
+	private ResponseBuilder response;
+	private final static String CAMINHO_PASTA = "C:/Users/RodrigoTenorio/";
+	
+	@POST
+	@Path("/resource")
+	@Produces("*/*")
+	public Response getParamento(@FormParam("fileName") String fileName){
+		return this.getResource(fileName);
+	}
+
+	/**
+	 * DOWNLOAD de Arquivos.
+	 *
+	 * @GET = Método HTTP
+	 * @Path = Caminho para acessar o método via http 
+	 * @Produces("/*") =Determinamos o media types que vamos usar para receber a resposta
+	 * @FormParam fileName
+	 * @return
+	 */
+	@GET
+	@Produces("*/*")
+	public Response getResource(String uri) {
+		uri = CAMINHO_PASTA + uri;
+		this.file = new File(uri);
+		this.response = Response.ok((Object) file);
+		response.header("Content-Disposition", "attachment;filename=" + file.getName());
+		return response.build();
+	}
+}
